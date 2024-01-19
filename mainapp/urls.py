4e5a2325ff45eb0668a4b5bca9f7eb4af4fcd820
Winzyss-code <dastergtm@gmail.com>
2024-01-19 21:35:19 +0600

    new pages static add

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path,include
+from django.urls import path
+
+from .views import index
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('',include('mainapp.urls')),
+    path('', index)
+    path('products/',products)
+
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request,'index.html')
+
+def products(request):
+    return render(request,'products.html')
